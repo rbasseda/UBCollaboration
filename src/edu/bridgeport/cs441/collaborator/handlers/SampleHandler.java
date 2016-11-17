649f8cb8ffeb194bd3ac214c20640840af09e1e6
rbasseda <rbasseda@cs.stonybrook.edu>
2016-11-17 00:38:28 -0500

    Adding stage and change

--- src/edu/bridgeport/cs441/collaborator/handlers/SampleHandler.java
+++ src/edu/bridgeport/cs441/collaborator/handlers/SampleHandler.java
@@ -8,6 +8,22 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
+*
+* KnowledgeBase.java
+*
+* Copyright (C) The Research Foundation of SUNY, 2015
+* All rights reserved.
+*
+* This software may be modified and distributed under the terms
+* of the BSD license.  See the LICENSE file for details.
+*
+* Created on: October 20th, 2011 
+* @author rbasseda
+*  
+* 
+*/
+
+/**
  * Our sample handler extends AbstractHandler, an IHandler base class.
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler

d6a4459b8956df0aeb3f7eaea16443499ccaf1f4
Reza Basseda <rbasseda@cs.stonybrook.edu>
2016-11-20 00:41:50 -0500

    Adding the license file.

--- src/edu/bridgeport/cs441/collaborator/handlers/SampleHandler.java
+++ src/edu/bridgeport/cs441/collaborator/handlers/SampleHandler.java
@@ -1,12 +1,4 @@
 package edu.bridgeport.cs441.collaborator.handlers;
-
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
-
 /**
 *
 * SampleHandler.java
@@ -17,14 +9,26 @@ import org.eclipse.jface.dialogs.MessageDialog;
 * This software may be modified and distributed under the terms
 * of the BSD license.  See the LICENSE file for details.
 *
-* Created on: November 17th, 2016 
-* @author Reza Basseda
-*  
+* Created on: November 17th, 2016   
 * 
 */
 
+
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
+ * @author Reza Basseda
+ * 
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */

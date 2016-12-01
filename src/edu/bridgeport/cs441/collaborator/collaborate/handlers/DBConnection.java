841e04ab247ffda763217b48d6a5564c288ba959
jmshe <jmshe@10.12.15.53>
2016-12-01 13:03:10 -0500

    test change

--- src/edu/bridgeport/cs441/collaborator/collaborate/handlers/DBConnection.java
+++ src/edu/bridgeport/cs441/collaborator/collaborate/handlers/DBConnection.java
@@ -1,5 +1,55 @@
 package edu.bridgeport.cs441.collaborator.collaborate.handlers;
-
-public class DBConnection {
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+/**
+ * @author Subha, Jay
+ *
+ */
+public class DBConnection //create class Connectivity
+{ 
+		
+		static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+		
+	    static final String DB_URL = "jdbc:mysql://localhost/demo";
+ 
+	    
+	    static final String USER = "root" ;
+	    static final String PASS = "root";
+	   
+	    public static void main(String[] args) 
+	    {
+	    	Connection conn = null;
+	    	try 
+	    	{
+	    		
+	    		Class.forName("com.mysql.jdbc.Driver");
+	    		System.out.println("Connecting to a selected database...");
+	    		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	    		System.out.println("Connected database successfully...");	    	
+                }
+	    	catch(SQLException se) 
+	    	{
+	    		se.printStackTrace();
+	    	}
+	    	catch(Exception e) 
+	    	{
+	    		e.printStackTrace();
+	    	}
+	    	finally  
+	    	{
+	    		try  
+	    		{
+	    			if(conn!=null)
+	    			conn.close(); 
+	    		}
+	    		catch(SQLException se)
+	    		{
+	    			se.printStackTrace();
+	    		}
+	    	}
+	    	System.out.println("Goodbye!"); 
+	    }
 }
+
